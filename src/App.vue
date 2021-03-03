d6563f3933d8e0ed109c8b4d49bd5f1edb9986fe
state-dogs-001 <darkness.state.001@gmail.com>
2021-03-03 22:15:59 +0700

    Test NASA API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app style="background-color: gray;">
+    <v-app-bar color="black" app fixed dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://api.nasa.gov/assets/img/favicons/favicon-192.png"
+          transition="scale-transition"
+          width="60"
+        />
+        <label
+          ><strong><h1>NASA API</h1></strong></label
+        >
+      </div>
+
+      <v-spacer />
+
+      <v-btn target="_blank" text @click="toTop">
+        <span class="mr-2">Back to the Top Page</span>
+        <v-icon>mdi-navigation</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main style="margin-top: 20px">
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/NasaAPI";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
+  },
+};
+</script>
---
+++ src/components/NasaAPI.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-row>
+        <v-col cols="4">
+          <v-img :src="dataList.url" class="ml-4" />
+          <hr style="width: 355px; margin-left: 16px; margin-top: 10px" />
+          <p class="font-weight-light" style="margin-left: 16px">
+            Credit : image from nasa (www.nasa.gov)
+          </p>
+        </v-col>
+        <v-col cols="8">
+          <h1>{{ dataList.title }}</h1>
+          <hr style="width: 620px; margin-top: 10px; margin-bottom: 10px" />
+          <h4>Date : {{ dataList.date }}</h4>
+          <br />
+          <p>{{ dataList.explanation }}</p>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => {
+    return {
+      dataList: {},
+    };
+  },
+  mounted() {
+    axios({
+      method: "GET",
+      url:
+        "https://api.nasa.gov/planetary/apod?api_key=daTLBdVyv7LlvH8bU7SEcpkdoTvPzeydwNMIzEiX",
+    })
+      .then((res) => {
+        this.dataList = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
